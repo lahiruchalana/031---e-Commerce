19051927958d7fc1fd6c76d111ee8bac64851b4b
lahiruchalana <lahiruchalanairesh@gmail.com>
2022-11-30 00:15:56 +0530

    APIGateway and ServiceRegistry

--- CartService/src/main/java/com/lciresh/cartservice/CartServiceApplication.java
+++ CartService/src/main/java/com/lciresh/cartservice/CartServiceApplication.java
@@ -2,15 +2,11 @@ package com.lciresh.cartservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
-//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
-@EnableEurekaClient
-//@ComponentScan("com.lciresh.cartservice.repository")
+//@EnableEurekaClient
 public class CartServiceApplication {
 
     public static void main(String[] args) {
